60824ce5cb729e8f8c66ff1335f584816445d57a
Cory <coadams9@gmail.com>
2018-12-26 18:28:55 -0500

    done

--- index.js
+++ index.js
@@ -5,3 +5,50 @@ const questions = [
 ]
 
 let question;
+
+
+function appendQuestion(question){
+  let qContainer = document.querySelector('.question-container')
+  qContainer.innerHTML = question.questionText
+}
+
+function askQuestionThen(time){
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise(function(resolve){
+    setTimeout(function(){
+      resolve(question)
+    }, time)
+  })
+}
+
+function removeQuestion(question) {
+  return new Promise(function(){
+    let qContainer = document.querySelector('.question-container')
+    qContainer.innerHTML = ""
+  })
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  return askQuestionThen(time).then(removeQuestion)
+}
+
+function trueAndFalseButtons(){
+  return btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+function toggleTrueAndFalseButtons(){
+  trueAndFalseButtons().forEach(function(buttons){
+    buttons.classList.toggle('hide')
+    // debugger
+  })
+}
+
+
+function displayQuestionOnClick(){
+  let button = document.querySelector('a')
+  return button.addEventListener('click', () => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
